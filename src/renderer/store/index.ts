ca16e75aa2d12efc51fc64946793e1adf6b28197
Alic Szecsei <aszecsei@gmail.com>
2019-12-22 23:02:21 -0800

    Enable redux devtools

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -1,6 +1,7 @@
 import { counterReducer } from './counter/reducers'
 import { guiReducer } from './gui/reducers'
 import { combineReducers, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 export const rootReducer = combineReducers({
   counter: counterReducer,
@@ -12,5 +13,5 @@ export type RootState = ReturnType<typeof rootReducer>
 export const counterSelector = (state: RootState) => state.counter
 export const guiSelector = (state: RootState) => state.gui
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 export default store
